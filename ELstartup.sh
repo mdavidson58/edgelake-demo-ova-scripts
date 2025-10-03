704ea5973d6f95083563a7f51857e74fabf88aeb
Mark Davidson <34720837+mdavidson58@users.noreply.github.com>
2025-10-03 12:36:02 -0400

    Update ELstartup.sh

--- ELstartup.sh
+++ ELstartup.sh
@@ -8,7 +8,7 @@ make up EDGELAKE_TYPE=operator2
 cd ~/Edgelake/query/docker-compose
 make up EDGELAKE_TYPE=query
 set -a
-source ./ELinstall.env
+source ~/Edgelake/ELinstall.env
 set +a
 IP_ADDR=$(ip -4 addr show "$NIC_TYPE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 docker run -d -p 8000:8000 -p 3001:3001   --name gui-1   --restart unless-stopped   -e REACT_APP_API_URL=http://"$IP_ADDR":8000   anylogco/remote-gui:bchain-beta
