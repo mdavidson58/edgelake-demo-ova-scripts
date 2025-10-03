2aca71a8fa4f0975d777f78a00be9e9df86e6edb
Mark Davidson <34720837+mdavidson58@users.noreply.github.com>
2025-10-03 13:00:54 -0400

    Update ELstartup.sh

--- ELstartup.sh
+++ ELstartup.sh
@@ -1,4 +1,6 @@
+
 #!/bin/bash
+# Start Edgelake Services
 cd ~/Edgelake/master/docker-compose
 make up EDGELAKE_TYPE=master
 cd ~/Edgelake/operator/docker-compose
@@ -7,8 +9,14 @@ cd ~/Edgelake/operator2/docker-compose
 make up EDGELAKE_TYPE=operator2
 cd ~/Edgelake/query/docker-compose
 make up EDGELAKE_TYPE=query
+
+# Load env variables
 set -a
 source ~/Edgelake/ELinstall.env
 set +a
 IP_ADDR=$(ip -4 addr show "$NIC_TYPE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# start sample grafana dashboard
+docker run -it -d -p 3000:3000 -e DATASOURCE_URL=http://"$IP_ADDR":32349 --name grafana anylogco/oh-grafana:latest
+# start new EL gui
 docker run -d -p 8000:8000 -p 3001:3001   --name gui-1   --restart unless-stopped   -e REACT_APP_API_URL=http://"$IP_ADDR":8000   anylogco/remote-gui:bchain-beta
