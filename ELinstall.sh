980ed996f49e4c0e6ce436e01ce936daafd72d38
Mark Davidson <34720837+mdavidson58@users.noreply.github.com>
2025-10-05 10:41:12 -0400

    Update ELinstall.sh

--- ELinstall.sh
+++ ELinstall.sh
@@ -4,6 +4,7 @@
 set -a
 source ./ELinstall.env
 set +a
+IP_ADDR=$(ip -4 addr show "$NIC_TYPE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 export NODE_TYPE="$2"
 echo "node_type=$NODE_TYPE"
@@ -29,8 +30,11 @@ do_install() {
   set -e
 
 # If docker, docker-compose and make are already installed via APT or another method, you can skip this step.
-#  sudo snap install docker
-#  sudo apt-get -y install make gettext git
+  sudo apt-get -y install docker-compose make gettext rsyslog
+
+# start syslog
+  sudo systemctl enable rsyslog
+  sudo systemctl start rsyslog
  
 # Grant non-root user permissions to use docker
 #  USER=`whoami`
@@ -62,7 +66,14 @@ do_install() {
   fi
   
   h="$(hostname)"
-
+  
+  # forward syslog to anylog
+  tee /etc/rsyslog.d/60-custom-forwarding.conf > /dev/null <<EOF
+template(name="MyCustomTemplate" type="string" string="<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag% %msg%\\n")
+*.* action(type="omfwd" target="${IP_ADDR}" port="32150" protocol="tcp" template="MyCustomTemplate")
+EOF
+  systemctl restart rsyslog
+      
   case "$NODE_TYPE" in
   master)
       ENV="docker-makefiles/edgelake_master.env"
@@ -71,8 +82,10 @@ do_install() {
       ensure_kv "LEDGER_CONN"   "${LEDGER_CONN}"         "$ENV"
       ensure_kv "TCP_BIND"      "${TCP_BIND}"            "$ENV"
       ensure_kv "REST_BIND"     "false"                  "$ENV"
-      ensure_kv "BROKER_BIND"   "true"                   "$ENV"
+      ensure_kv "BROKER_BIND"   "false"                  "$ENV"
       ensure_kv "NIC_TYPE"      "${NIC_TYPE}"            "$ENV"
+      ensure_kv "MONITOR_NODES" "true"                   "$ENV"
+      ensure_kv "STORE_MONITORING" "true"                "$ENV"
       
       make up EDGELAKE_TYPE="${NODE_TYPE}"
       ;;
@@ -84,28 +97,35 @@ do_install() {
       ensure_kv "LEDGER_CONN"   "${LEDGER_CONN}"         "$ENV"
       ensure_kv "TCP_BIND"      "${TCP_BIND}"            "$ENV"
       ensure_kv "REST_BIND"     "false"                  "$ENV"
-      ensure_kv "BROKER_BIND"   "true"                   "$ENV"
+      ensure_kv "BROKER_BIND"   "false"                  "$ENV"
       ensure_kv "NIC_TYPE"      "${NIC_TYPE}"            "$ENV"
       ensure_kv "CLUSTER_NAME"  "${h}-operator-cluster"  "$ENV"
       ensure_kv "ENABLE_MQTT"   "true"                   "$ENV"
       ensure_kv "MQTT_BROKER"   "172.104.228.251"        "$ENV"
+      ensure_kv "MONITOR_NODES" "true"                   "$ENV"
+      ensure_kv "STORE_MONITORING" "true"                "$ENV"
+      ensure_kv "SYSLOG_MONITORING" "true"               "$ENV"
+
      
       make up EDGELAKE_TYPE="${NODE_TYPE}"
       ;;
 
   operator2)
       ENV="docker-makefiles/edgelake_operator2.env"
-      ensure_kv "NODE_NAME"     "${h}-operator2"          "$ENV"
+      ensure_kv "NODE_NAME"     "${h}-operator2"         "$ENV"
       ensure_kv "COMPANY_NAME"  "${COMPANY_NAME}"        "$ENV"
       ensure_kv "LEDGER_CONN"   "${LEDGER_CONN}"         "$ENV"
       ensure_kv "TCP_BIND"      "${TCP_BIND}"            "$ENV"
       ensure_kv "REST_BIND"     "false"                  "$ENV"
-      ensure_kv "BROKER_BIND"   "true"                   "$ENV"
+      ensure_kv "BROKER_BIND"   "false"                  "$ENV"
       ensure_kv "NIC_TYPE"      "${NIC_TYPE}"            "$ENV"
       ensure_kv "CLUSTER_NAME"  "${h}-operator2-cluster" "$ENV"
       ensure_kv "ENABLE_MQTT"   "true"                   "$ENV"
       ensure_kv "MQTT_BROKER"   "172.104.228.251"        "$ENV"
-     
+      ensure_kv "MONITOR_NODES" "true"                   "$ENV"
+      ensure_kv "STORE_MONITORING" "true"                "$ENV"
+      ensure_kv "SYSLOG_MONITORING" "false"              "$ENV"
+
       make up EDGELAKE_TYPE="${NODE_TYPE}"
       ;;
      
@@ -116,14 +136,17 @@ do_install() {
       ensure_kv "LEDGER_CONN"   "${LEDGER_CONN}"         "$ENV"
       ensure_kv "TCP_BIND"      "${TCP_BIND}"            "$ENV"
       ensure_kv "REST_BIND"     "false"                  "$ENV"
-      ensure_kv "BROKER_BIND"   "false"                   "$ENV"
+      ensure_kv "BROKER_BIND"   "false"                  "$ENV"
       ensure_kv "NIC_TYPE"      "${NIC_TYPE}"            "$ENV"
       ensure_kv "REMOTE_CLI"    "true"                   "$ENV"
+      ensure_kv "MONITOR_NODES" "true"                   "$ENV"
+      ensure_kv "STORE_MONITORING" "true"                "$ENV"
       
       make up EDGELAKE_TYPE="${NODE_TYPE}"
-      # run remote gui
-      IP_ADDR=$(ip -4 addr show "$NIC_TYPE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    
+      # run remote gui and dashboard
       docker run -d -p 8000:8000 -p 3001:3001   --name gui-1   --restart unless-stopped   -e REACT_APP_API_URL=http://"$IP_ADDR":8000   anylogco/remote-gui:bchain-beta
+      docker run -it -d -p 3000:3000 --restart unless-stopped -e DATASOURCE_URL=http://"$IP_ADDR":32349 --name grafana anylogco/oh-grafana:latest
       ;;
 
   *)
