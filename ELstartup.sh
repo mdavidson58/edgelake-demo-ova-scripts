81a0bd87db94a16f2979355238b01171523bbc78
Mark Davidson <34720837+mdavidson58@users.noreply.github.com>
2025-10-16 17:09:51 -0400

    Update GUI container image tag to 'beta'

--- ELstartup.sh
+++ ELstartup.sh
@@ -19,4 +19,4 @@ IP_ADDR=$(ip -4 addr show "$NIC_TYPE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 # start sample grafana dashboard
 docker run -it -d -p 3000:3000 --restart unless-stopped -e DATASOURCE_URL=http://"$IP_ADDR":32349 --name grafana anylogco/oh-grafana:latest
 # start new EL gui
-docker run -d -p 8000:8000 -p 3001:3001   --name gui-1   --restart unless-stopped   -e REACT_APP_API_URL=http://"$IP_ADDR":8000   anylogco/remote-gui:bchain-beta
+docker run -d -p 8000:8000 -p 3001:3001   --name gui-1   --restart unless-stopped   -e REACT_APP_API_URL=http://"$IP_ADDR":8000   anylogco/remote-gui:beta
